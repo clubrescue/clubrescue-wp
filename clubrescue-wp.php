56d4d27b7221738ce47ed6b755e0b782ad51af15
Ruud Borghouts <mail@ruudborghouts.nl>
2020-07-14 13:09:50 +0200

    Initial code with option settings
    Code can now be adjusted with user settings using the new options page of the plugin.

--- clubrescue-wp.php
+++ clubrescue-wp.php
@@ -14,7 +14,10 @@
  * Text Domain: The gettext text domain of the plugin. More information can be found in the Text Domain section of the How to Internationalize your Plugin page.
  * Domain Path: The domain path lets WordPress know where to find the translations. More information can be found in the Domain Path section of the How to Internationalize your Plugin page.
  */
- 
+
+// Retrieve option value's:
+$crwp_settings = get_option( 'crwp_settings' ); // Array of All Options
+
 // Use the Plugin Update Checker add-on to receive automatic update notifications and one-click upgrades from GitHub.
 require 'plugin-update-checker/plugin-update-checker.php';
 $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
@@ -23,7 +26,7 @@ $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
 	'clubrescue-wp'
 );
 	//Optional: Set the branch that contains the stable release.
-	$myUpdateChecker->setBranch('master');
+	$myUpdateChecker->setBranch($crwp_settings["crwp_branch"]);
 		//Alternative: If you only want updates from release assets, call the enableReleaseAssets() method instead of the branch check above.
 		//$myUpdateChecker->getVcsApi()->enableReleaseAssets();
 	//Optional: If you're using a private repository, specify the access token like this:
@@ -37,29 +40,43 @@ $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
 
 define( 'CRWP_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 require_once CRWP_PLUGIN_DIR . '/clubrescue-wp-admin.php';
+//register_activation_hook( __FILE__, 'crwp_activate' );
 
 function add_plugin_links( $links ) {
+	// Retrieve option value's:
+	$crwp_settings = get_option( 'crwp_settings' ); // Array of All Options
+	
 	$links[] = '<a href="' .
 		admin_url( 'options-general.php?page=cr-wp' ) .
 		'">' . __('Settings') . '</a>';
+	
+	if($crwp_settings["crwp_links"] === "true" ) {
+	$links[] .= '<a href="../' .
+		$crwp_settings["crwp_otap"] .
+		'" target="_blank">' . __('Your Club.Rescue') . '</a>';	
+	}
+	
 	return $links;
 }
 add_filter('plugin_action_links_'.plugin_basename(__FILE__), 'add_plugin_links');
 
 // Trigger C.R O365 authentication
 function CRWP_O365_HEADERS() {
+	// Retrieve option value's:
+	$crwp_settings = get_option( 'crwp_settings' ); // Array of All Options
+	
 	//if(is_page('mijn-trb-nu')) {
-	if(is_page(array('mijn-trb-nu', 'work', 'about', 'contact'))) {
+	if(is_page(explode(", ",$crwp_settings["crwp_pages"]))) {
 		session_start();
 
 		if (!isset($_SESSION['token'])) {
 			$_SESSION['O365_REDIRECT'] = $_SERVER['REQUEST_URI'];
-			include $_SERVER['DOCUMENT_ROOT'] . '/clubredders/auth.php';
+			include $_SERVER['DOCUMENT_ROOT'] . '/' . $crwp_settings["crwp_otap"] . '/auth.php';
 		}
 
-		require_once $_SERVER['DOCUMENT_ROOT'] . '/clubredders/util/msgraph/user.class.php';
-		require_once $_SERVER['DOCUMENT_ROOT'] . '/clubredders/util/utility.class.php';
-		require_once $_SERVER['DOCUMENT_ROOT'] . '/clubredders/util/database.class.php';
+		require_once $_SERVER['DOCUMENT_ROOT'] . '/' . $crwp_settings["crwp_otap"] . '/util/msgraph/user.class.php';
+		require_once $_SERVER['DOCUMENT_ROOT'] . '/' . $crwp_settings["crwp_otap"] . '/util/utility.class.php';
+		require_once $_SERVER['DOCUMENT_ROOT'] . '/' . $crwp_settings["crwp_otap"] . '/util/database.class.php';
 
 		$user = new MSGraphUser($_SESSION['token']);
 
@@ -70,10 +87,13 @@ add_action( 'template_redirect', 'CRWP_O365_HEADERS' ); // Will trigger the CR-O
 //add_filter('wp_headers', 'CRWP_O365_HEADERS'); // Will trigger the CR-O365 login for the entire site when activated.
 
 function CRWP_MyCR_data_loader( $atts = '' ){
+	// Retrieve option value's:
+	$crwp_settings = get_option( 'crwp_settings' ); // Array of All Options
+	
     $value = shortcode_atts( array(
-        'otap' => 'clubrescue',
-        'source' => 'mycr-attributes',
-        'table' => 'memberTable',
+        'otap' => $crwp_settings["crwp_otap"],
+        'source' => $crwp_settings["crwp_source"],
+        'table' => $crwp_settings["crwp_variable"],
     ), $atts );
 	
     //$user = wp_get_current_user(); // Disable this line when using the CR-O365 authentication instead of the legacy WP(-O365) authentication.
