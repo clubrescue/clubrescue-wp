6e1a26625439102f36b34255d34dbb167507024e
Ruud Borghouts <mail@ruudborghouts.nl>
2020-10-01 08:20:16 +0200

    PUC upgrade
    Plugin Update Checker upgrade from 4.9 to 4.10 with support for the new auto-update feature introduced in WordPress 5.5.

--- clubrescue-wp-admin.php
+++ clubrescue-wp-admin.php
@@ -49,6 +49,16 @@ Please try again later.',
 					submit_button();
 				?>
 			</form>
+			
+			<h3><?php echo esc_html__( 'Shortcode instructions' ); ?></h3>
+			<p><?php echo 'The following shortcodes are available (only) on pages listed under the \'My Club.Rescue pages\' setting;<br>
+			[crwp_mycr] will return the default source and variable data from My Club.Rescue as configured in the settings source and variable.<br>
+			The shortcode has three attributes that can be used to change the returned data:<br>
+			[crwp_mycr otap="clubrescue" source="mycr-attributes" table="lidTable"]<br>
+			otap defines the folder of the Club.Rescue installation that is used. Usefull for whitelabeling or testing installations.
+			source defines the My C.R submodule to include.
+			table defines the data set from the submodule to return.'; ?></p>
+			
 		</div>
 	<?php }
 
